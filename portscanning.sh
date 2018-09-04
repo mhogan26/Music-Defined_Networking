59783dce0f037f5e4b591775dc75a81e44689168
mhogan26 <mhogan26@slu.edu>
2018-09-04 13:05:36 -0500

    add port scanning script

---
+++ portscanning.sh
@@ -0,0 +1,638 @@
+#!/bin/bash
+
+eval $(nc -z 10.0.1.4 8600)
+sleep .1
+eval $(nc -z 10.0.1.4 8601)
+sleep .1
+eval $(nc -z 10.0.1.4 8602)
+sleep .1
+eval $(nc -z 10.0.1.4 8603)
+sleep .1
+eval $(nc -z 10.0.1.4 8604)
+sleep .1
+eval $(nc -z 10.0.1.4 8605)
+sleep .1
+eval $(nc -z 10.0.1.4 8606)
+sleep .1
+eval $(nc -z 10.0.1.4 8607)
+sleep .1
+eval $(nc -z 10.0.1.4 8608)
+sleep .1
+eval $(nc -z 10.0.1.4 8609)
+sleep .1
+eval $(nc -z 10.0.1.4 8610)
+sleep .1
+eval $(nc -z 10.0.1.4 8611)
+sleep .1
+eval $(nc -z 10.0.1.4 8612)
+sleep .1
+eval $(nc -z 10.0.1.4 8613)
+sleep .1
+eval $(nc -z 10.0.1.4 8614)
+sleep .1
+eval $(nc -z 10.0.1.4 8615)
+sleep .1
+eval $(nc -z 10.0.1.4 8616)
+sleep .1
+eval $(nc -z 10.0.1.4 8617)
+sleep .1
+eval $(nc -z 10.0.1.4 8618)
+sleep .1
+eval $(nc -z 10.0.1.4 8619)
+sleep .1
+eval $(nc -z 10.0.1.4 8620)
+sleep .1
+eval $(nc -z 10.0.1.4 8621)
+sleep .1
+eval $(nc -z 10.0.1.4 8622)
+sleep .1
+eval $(nc -z 10.0.1.4 8623)
+sleep .1
+eval $(nc -z 10.0.1.4 8624)
+sleep .1
+eval $(nc -z 10.0.1.4 8625)
+sleep .1
+eval $(nc -z 10.0.1.4 8626)
+sleep .1
+eval $(nc -z 10.0.1.4 8627)
+sleep .1
+eval $(nc -z 10.0.1.4 8628)
+sleep .1
+eval $(nc -z 10.0.1.4 8629)
+sleep .1
+eval $(nc -z 10.0.1.4 8630)
+sleep .1
+eval $(nc -z 10.0.1.4 8631)
+sleep .1
+eval $(nc -z 10.0.1.4 8632)
+sleep .1
+eval $(nc -z 10.0.1.4 8633)
+sleep .1
+eval $(nc -z 10.0.1.4 8634)
+sleep .1
+eval $(nc -z 10.0.1.4 8635)
+sleep .1
+eval $(nc -z 10.0.1.4 8636)
+sleep .1
+eval $(nc -z 10.0.1.4 8637)
+sleep .1
+eval $(nc -z 10.0.1.4 8638)
+sleep .1
+eval $(nc -z 10.0.1.4 8639)
+sleep .1
+eval $(nc -z 10.0.1.4 8640)
+sleep .1
+eval $(nc -z 10.0.1.4 8641)
+sleep .1
+eval $(nc -z 10.0.1.4 8642)
+sleep .1
+eval $(nc -z 10.0.1.4 8643)
+sleep .1
+eval $(nc -z 10.0.1.4 8644)
+sleep .1
+eval $(nc -z 10.0.1.4 8645)
+sleep .1
+eval $(nc -z 10.0.1.4 8646)
+sleep .1
+eval $(nc -z 10.0.1.4 8647)
+sleep .1
+eval $(nc -z 10.0.1.4 8648)
+sleep .1
+eval $(nc -z 10.0.1.4 8649)
+sleep .1
+eval $(nc -z 10.0.1.4 8650)
+sleep .1
+eval $(nc -z 10.0.1.4 8651)
+sleep .1
+eval $(nc -z 10.0.1.4 8652)
+sleep .1
+eval $(nc -z 10.0.1.4 8653)
+sleep .1
+eval $(nc -z 10.0.1.4 8654)
+sleep .1
+eval $(nc -z 10.0.1.4 8655)
+sleep .1
+eval $(nc -z 10.0.1.4 8656)
+sleep .1
+eval $(nc -z 10.0.1.4 8657)
+sleep .1
+eval $(nc -z 10.0.1.4 8658)
+sleep .1
+eval $(nc -z 10.0.1.4 8659)
+sleep .1
+eval $(nc -z 10.0.1.4 8660)
+sleep .1
+eval $(nc -z 10.0.1.4 8661)
+sleep .1
+eval $(nc -z 10.0.1.4 8662)
+sleep .1
+eval $(nc -z 10.0.1.4 8663)
+sleep .1
+eval $(nc -z 10.0.1.4 8664)
+sleep .1
+eval $(nc -z 10.0.1.4 8665)
+sleep .1
+eval $(nc -z 10.0.1.4 8666)
+sleep .1
+eval $(nc -z 10.0.1.4 8667)
+sleep .1
+eval $(nc -z 10.0.1.4 8668)
+sleep .1
+eval $(nc -z 10.0.1.4 8669)
+sleep .1
+eval $(nc -z 10.0.1.4 8670)
+sleep .1
+eval $(nc -z 10.0.1.4 8671)
+sleep .1
+eval $(nc -z 10.0.1.4 8672)
+sleep .1
+eval $(nc -z 10.0.1.4 8673)
+sleep .1
+eval $(nc -z 10.0.1.4 8674)
+sleep .1
+eval $(nc -z 10.0.1.4 8675)
+sleep .1
+eval $(nc -z 10.0.1.4 8676)
+sleep .1
+eval $(nc -z 10.0.1.4 8677)
+sleep .1
+eval $(nc -z 10.0.1.4 8678)
+sleep .1
+eval $(nc -z 10.0.1.4 8679)
+sleep .1
+eval $(nc -z 10.0.1.4 8680)
+sleep .1
+eval $(nc -z 10.0.1.4 8681)
+sleep .1
+eval $(nc -z 10.0.1.4 8682)
+sleep .1
+eval $(nc -z 10.0.1.4 8683)
+sleep .1
+eval $(nc -z 10.0.1.4 8684)
+sleep .1
+eval $(nc -z 10.0.1.4 8685)
+sleep .1
+eval $(nc -z 10.0.1.4 8686)
+sleep .1
+eval $(nc -z 10.0.1.4 8687)
+sleep .1
+eval $(nc -z 10.0.1.4 8688)
+sleep .1
+eval $(nc -z 10.0.1.4 8689)
+sleep .1
+eval $(nc -z 10.0.1.4 8690)
+sleep .1
+eval $(nc -z 10.0.1.4 8691)
+sleep .1
+eval $(nc -z 10.0.1.4 8692)
+sleep .1
+eval $(nc -z 10.0.1.4 8693)
+sleep .1
+eval $(nc -z 10.0.1.4 8694)
+sleep .1
+eval $(nc -z 10.0.1.4 8695)
+sleep .1
+eval $(nc -z 10.0.1.4 8696)
+sleep .1
+eval $(nc -z 10.0.1.4 8697)
+sleep .1
+eval $(nc -z 10.0.1.4 8698)
+sleep .1
+eval $(nc -z 10.0.1.4 8699)
+sleep .1
+eval $(nc -z 10.0.1.4 8700)
+sleep .1
+eval $(nc -z 10.0.1.4 8701)
+sleep .1
+eval $(nc -z 10.0.1.4 8702)
+sleep .1
+eval $(nc -z 10.0.1.4 8703)
+sleep .1
+eval $(nc -z 10.0.1.4 8704)
+sleep .1
+eval $(nc -z 10.0.1.4 8705)
+sleep .1
+eval $(nc -z 10.0.1.4 8706)
+sleep .1
+eval $(nc -z 10.0.1.4 8707)
+sleep .1
+eval $(nc -z 10.0.1.4 8708)
+sleep .1
+eval $(nc -z 10.0.1.4 8709)
+sleep .1
+eval $(nc -z 10.0.1.4 8710)
+sleep .1
+eval $(nc -z 10.0.1.4 8711)
+sleep .1
+eval $(nc -z 10.0.1.4 8712)
+sleep .1
+eval $(nc -z 10.0.1.4 8713)
+sleep .1
+eval $(nc -z 10.0.1.4 8714)
+sleep .1
+eval $(nc -z 10.0.1.4 8715)
+sleep .1
+eval $(nc -z 10.0.1.4 8716)
+sleep .1
+eval $(nc -z 10.0.1.4 8717)
+sleep .1
+eval $(nc -z 10.0.1.4 8718)
+sleep .1
+eval $(nc -z 10.0.1.4 8719)
+sleep .1
+eval $(nc -z 10.0.1.4 8720)
+sleep .1
+eval $(nc -z 10.0.1.4 8721)
+sleep .1
+eval $(nc -z 10.0.1.4 8722)
+sleep .1
+eval $(nc -z 10.0.1.4 8723)
+sleep .1
+eval $(nc -z 10.0.1.4 8724)
+sleep .1
+eval $(nc -z 10.0.1.4 8725)
+sleep .1
+eval $(nc -z 10.0.1.4 8726)
+sleep .1
+eval $(nc -z 10.0.1.4 8727)
+sleep .1
+eval $(nc -z 10.0.1.4 8728)
+sleep .1
+eval $(nc -z 10.0.1.4 8729)
+sleep .1
+eval $(nc -z 10.0.1.4 8730)
+sleep .1
+eval $(nc -z 10.0.1.4 8731)
+sleep .1
+eval $(nc -z 10.0.1.4 8732)
+sleep .1
+eval $(nc -z 10.0.1.4 8733)
+sleep .1
+eval $(nc -z 10.0.1.4 8734)
+sleep .1
+eval $(nc -z 10.0.1.4 8735)
+sleep .1
+eval $(nc -z 10.0.1.4 8736)
+sleep .1
+eval $(nc -z 10.0.1.4 8737)
+sleep .1
+eval $(nc -z 10.0.1.4 8738)
+sleep .1
+eval $(nc -z 10.0.1.4 8739)
+sleep .1
+eval $(nc -z 10.0.1.4 8740)
+sleep .1
+eval $(nc -z 10.0.1.4 8741)
+sleep .1
+eval $(nc -z 10.0.1.4 8742)
+sleep .1
+eval $(nc -z 10.0.1.4 8743)
+sleep .1
+eval $(nc -z 10.0.1.4 8744)
+sleep .1
+eval $(nc -z 10.0.1.4 8745)
+sleep .1
+eval $(nc -z 10.0.1.4 8746)
+sleep .1
+eval $(nc -z 10.0.1.4 8747)
+sleep .1
+eval $(nc -z 10.0.1.4 8748)
+sleep .1
+eval $(nc -z 10.0.1.4 8749)
+sleep .1
+eval $(nc -z 10.0.1.4 8750)
+sleep .1
+eval $(nc -z 10.0.1.4 8751)
+sleep .1
+eval $(nc -z 10.0.1.4 8752)
+sleep .1
+eval $(nc -z 10.0.1.4 8753)
+sleep .1
+eval $(nc -z 10.0.1.4 8754)
+sleep .1
+eval $(nc -z 10.0.1.4 8755)
+sleep .1
+eval $(nc -z 10.0.1.4 8756)
+sleep .1
+eval $(nc -z 10.0.1.4 8757)
+sleep .1
+eval $(nc -z 10.0.1.4 8758)
+sleep .1
+eval $(nc -z 10.0.1.4 8759)
+sleep .1
+eval $(nc -z 10.0.1.4 8760)
+sleep .1
+eval $(nc -z 10.0.1.4 8761)
+sleep .1
+eval $(nc -z 10.0.1.4 8762)
+sleep .1
+eval $(nc -z 10.0.1.4 8763)
+sleep .1
+eval $(nc -z 10.0.1.4 8764)
+sleep .1
+eval $(nc -z 10.0.1.4 8765)
+sleep .1
+eval $(nc -z 10.0.1.4 8766)
+sleep .1
+eval $(nc -z 10.0.1.4 8767)
+sleep .1
+eval $(nc -z 10.0.1.4 8768)
+sleep .1
+eval $(nc -z 10.0.1.4 8769)
+sleep .1
+eval $(nc -z 10.0.1.4 8770)
+sleep .1
+eval $(nc -z 10.0.1.4 8771)
+sleep .1
+eval $(nc -z 10.0.1.4 8772)
+sleep .1
+eval $(nc -z 10.0.1.4 8773)
+sleep .1
+eval $(nc -z 10.0.1.4 8774)
+sleep .1
+eval $(nc -z 10.0.1.4 8775)
+sleep .1
+eval $(nc -z 10.0.1.4 8776)
+sleep .1
+eval $(nc -z 10.0.1.4 8777)
+sleep .1
+eval $(nc -z 10.0.1.4 8778)
+sleep .1
+eval $(nc -z 10.0.1.4 8779)
+sleep .1
+eval $(nc -z 10.0.1.4 8780)
+sleep .1
+eval $(nc -z 10.0.1.4 8781)
+sleep .1
+eval $(nc -z 10.0.1.4 8782)
+sleep .1
+eval $(nc -z 10.0.1.4 8783)
+sleep .1
+eval $(nc -z 10.0.1.4 8784)
+sleep .1
+eval $(nc -z 10.0.1.4 8785)
+sleep .1
+eval $(nc -z 10.0.1.4 8786)
+sleep .1
+eval $(nc -z 10.0.1.4 8787)
+sleep .1
+eval $(nc -z 10.0.1.4 8788)
+sleep .1
+eval $(nc -z 10.0.1.4 8789)
+sleep .1
+eval $(nc -z 10.0.1.4 8790)
+sleep .1
+eval $(nc -z 10.0.1.4 8791)
+sleep .1
+eval $(nc -z 10.0.1.4 8792)
+sleep .1
+eval $(nc -z 10.0.1.4 8793)
+sleep .1
+eval $(nc -z 10.0.1.4 8794)
+sleep .1
+eval $(nc -z 10.0.1.4 8795)
+sleep .1
+eval $(nc -z 10.0.1.4 8796)
+sleep .1
+eval $(nc -z 10.0.1.4 8797)
+sleep .1
+eval $(nc -z 10.0.1.4 8798)
+sleep .1
+eval $(nc -z 10.0.1.4 8799)
+sleep .1
+eval $(nc -z 10.0.1.4 8800)
+sleep .1
+eval $(nc -z 10.0.1.4 8801)
+sleep .1
+eval $(nc -z 10.0.1.4 8802)
+sleep .1
+eval $(nc -z 10.0.1.4 8803)
+sleep .1
+eval $(nc -z 10.0.1.4 8804)
+sleep .1
+eval $(nc -z 10.0.1.4 8805)
+sleep .1
+eval $(nc -z 10.0.1.4 8806)
+sleep .1
+eval $(nc -z 10.0.1.4 8807)
+sleep .1
+eval $(nc -z 10.0.1.4 8808)
+sleep .1
+eval $(nc -z 10.0.1.4 8809)
+sleep .1
+eval $(nc -z 10.0.1.4 8810)
+sleep .1
+eval $(nc -z 10.0.1.4 8811)
+sleep .1
+eval $(nc -z 10.0.1.4 8812)
+sleep .1
+eval $(nc -z 10.0.1.4 8813)
+sleep .1
+eval $(nc -z 10.0.1.4 8814)
+sleep .1
+eval $(nc -z 10.0.1.4 8815)
+sleep .1
+eval $(nc -z 10.0.1.4 8816)
+sleep .1
+eval $(nc -z 10.0.1.4 8817)
+sleep .1
+eval $(nc -z 10.0.1.4 8818)
+sleep .1
+eval $(nc -z 10.0.1.4 8819)
+sleep .1
+eval $(nc -z 10.0.1.4 8820)
+sleep .1
+eval $(nc -z 10.0.1.4 8821)
+sleep .1
+eval $(nc -z 10.0.1.4 8822)
+sleep .1
+eval $(nc -z 10.0.1.4 8823)
+sleep .1
+eval $(nc -z 10.0.1.4 8824)
+sleep .1
+eval $(nc -z 10.0.1.4 8825)
+sleep .1
+eval $(nc -z 10.0.1.4 8826)
+sleep .1
+eval $(nc -z 10.0.1.4 8827)
+sleep .1
+eval $(nc -z 10.0.1.4 8828)
+sleep .1
+eval $(nc -z 10.0.1.4 8829)
+sleep .1
+eval $(nc -z 10.0.1.4 8830)
+sleep .1
+eval $(nc -z 10.0.1.4 8831)
+sleep .1
+eval $(nc -z 10.0.1.4 8832)
+sleep .1
+eval $(nc -z 10.0.1.4 8833)
+sleep .1
+eval $(nc -z 10.0.1.4 8834)
+sleep .1
+eval $(nc -z 10.0.1.4 8835)
+sleep .1
+eval $(nc -z 10.0.1.4 8836)
+sleep .1
+eval $(nc -z 10.0.1.4 8837)
+sleep .1
+eval $(nc -z 10.0.1.4 8838)
+sleep .1
+eval $(nc -z 10.0.1.4 8839)
+sleep .1
+eval $(nc -z 10.0.1.4 8840)
+sleep .1
+eval $(nc -z 10.0.1.4 8841)
+sleep .1
+eval $(nc -z 10.0.1.4 8842)
+sleep .1
+eval $(nc -z 10.0.1.4 8843)
+sleep .1
+eval $(nc -z 10.0.1.4 8844)
+sleep .1
+eval $(nc -z 10.0.1.4 8845)
+sleep .1
+eval $(nc -z 10.0.1.4 8846)
+sleep .1
+eval $(nc -z 10.0.1.4 8847)
+sleep .1
+eval $(nc -z 10.0.1.4 8848)
+sleep .1
+eval $(nc -z 10.0.1.4 8849)
+sleep .1
+eval $(nc -z 10.0.1.4 8850)
+sleep .1
+eval $(nc -z 10.0.1.4 8851)
+sleep .1
+eval $(nc -z 10.0.1.4 8852)
+sleep .1
+eval $(nc -z 10.0.1.4 8853)
+sleep .1
+eval $(nc -z 10.0.1.4 8854)
+sleep .1
+eval $(nc -z 10.0.1.4 8855)
+sleep .1
+eval $(nc -z 10.0.1.4 8856)
+sleep .1
+eval $(nc -z 10.0.1.4 8857)
+sleep .1
+eval $(nc -z 10.0.1.4 8858)
+sleep .1
+eval $(nc -z 10.0.1.4 8859)
+sleep .1
+eval $(nc -z 10.0.1.4 8860)
+sleep .1
+eval $(nc -z 10.0.1.4 8861)
+sleep .1
+eval $(nc -z 10.0.1.4 8862)
+sleep .1
+eval $(nc -z 10.0.1.4 8863)
+sleep .1
+eval $(nc -z 10.0.1.4 8864)
+sleep .1
+eval $(nc -z 10.0.1.4 8865)
+sleep .1
+eval $(nc -z 10.0.1.4 8866)
+sleep .1
+eval $(nc -z 10.0.1.4 8867)
+sleep .1
+eval $(nc -z 10.0.1.4 8868)
+sleep .1
+eval $(nc -z 10.0.1.4 8869)
+sleep .1
+eval $(nc -z 10.0.1.4 8870)
+sleep .1
+eval $(nc -z 10.0.1.4 8871)
+sleep .1
+eval $(nc -z 10.0.1.4 8872)
+sleep .1
+eval $(nc -z 10.0.1.4 8873)
+sleep .1
+eval $(nc -z 10.0.1.4 8874)
+sleep .1
+eval $(nc -z 10.0.1.4 8875)
+sleep .1
+eval $(nc -z 10.0.1.4 8876)
+sleep .1
+eval $(nc -z 10.0.1.4 8877)
+sleep .1
+eval $(nc -z 10.0.1.4 8878)
+sleep .1
+eval $(nc -z 10.0.1.4 8879)
+sleep .1
+eval $(nc -z 10.0.1.4 8880)
+sleep .1
+eval $(nc -z 10.0.1.4 8881)
+sleep .1
+eval $(nc -z 10.0.1.4 8882)
+sleep .1
+eval $(nc -z 10.0.1.4 8883)
+sleep .1
+eval $(nc -z 10.0.1.4 8884)
+sleep .1
+eval $(nc -z 10.0.1.4 8885)
+sleep .1
+eval $(nc -z 10.0.1.4 8886)
+sleep .1
+eval $(nc -z 10.0.1.4 8887)
+sleep .1
+eval $(nc -z 10.0.1.4 8888)
+sleep .1
+eval $(nc -z 10.0.1.4 8889)
+sleep .1
+eval $(nc -z 10.0.1.4 8890)
+sleep .1
+eval $(nc -z 10.0.1.4 8891)
+sleep .1
+eval $(nc -z 10.0.1.4 8892)
+sleep .1
+eval $(nc -z 10.0.1.4 8893)
+sleep .1
+eval $(nc -z 10.0.1.4 8894)
+sleep .1
+eval $(nc -z 10.0.1.4 8895)
+sleep .1
+eval $(nc -z 10.0.1.4 8896)
+sleep .1
+eval $(nc -z 10.0.1.4 8897)
+sleep .1
+eval $(nc -z 10.0.1.4 8898)
+sleep .1
+eval $(nc -z 10.0.1.4 8899)
+sleep .1
+eval $(nc -z 10.0.1.4 8900)
+sleep .1
+eval $(nc -z 10.0.1.4 8901)
+sleep .1
+eval $(nc -z 10.0.1.4 8902)
+sleep .1
+eval $(nc -z 10.0.1.4 8903)
+sleep .1
+eval $(nc -z 10.0.1.4 8904)
+sleep .1
+eval $(nc -z 10.0.1.4 8905)
+sleep .1
+eval $(nc -z 10.0.1.4 8906)
+sleep .1
+eval $(nc -z 10.0.1.4 8907)
+sleep .1
+eval $(nc -z 10.0.1.4 8908)
+sleep .1
+eval $(nc -z 10.0.1.4 8909)
+sleep .1
+eval $(nc -z 10.0.1.4 8910)
+sleep .1
+eval $(nc -z 10.0.1.4 8911)
+sleep .1
+eval $(nc -z 10.0.1.4 8912)
+sleep .1
+eval $(nc -z 10.0.1.4 8913)
+sleep .1
+eval $(nc -z 10.0.1.4 8914)
+sleep .1
+eval $(nc -z 10.0.1.4 8915)
+sleep .1
+eval $(nc -z 10.0.1.4 8916)
+sleep .1
+
+
